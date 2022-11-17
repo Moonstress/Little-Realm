c4178e3d5e28c4b7d0e8632df9735c84d575ba35
Moonstress <118330363+Moonstress@users.noreply.github.com>
2022-11-17 14:09:01 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,83 @@
-const slider = document.querySelector('.gallery');
-let isDown = false;
-let startX;
-let scrollLeft;
+const $ = selector => {
+  return document.querySelector(selector);
+};
 
-slider.addEventListener('mousedown', e => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
+function next() {
+  if ($(".hide")) {
+    $(".hide").remove(); 
+  }
+
+  /* Step */
+
+  if ($(".prev")) {
+    $(".prev").classList.add("hide");
+    $(".prev").classList.remove("prev");
+  }
+
+  $(".act").classList.add("prev");
+  $(".act").classList.remove("act");
+
+  $(".next").classList.add("act");
+  $(".next").classList.remove("next");
+
+  /* New Next */
+
+  $(".new-next").classList.remove("new-next");
+
+  const addedEl = document.createElement('li');
+
+  $(".list").appendChild(addedEl);
+  addedEl.classList.add("next","new-next");
+}
+
+function prev() {
+  $(".new-next").remove();
+    
+  /* Step */
+
+  $(".next").classList.add("new-next");
+
+  $(".act").classList.add("next");
+  $(".act").classList.remove("act");
+
+  $(".prev").classList.add("act");
+  $(".prev").classList.remove("prev");
+
+  /* New Prev */
+
+  $(".hide").classList.add("prev");
+  $(".hide").classList.remove("hide");
+
+  const addedEl = document.createElement('li');
+
+  $(".list").insertBefore(addedEl, $(".list").firstChild);
+  addedEl.classList.add("hide");
+}
+
+slide = element => {
+  /* Next slide */
+  
+  if (element.classList.contains('next')) {
+    next();
+    
+  /* Previous slide */
+    
+  } else if (element.classList.contains('prev')) {
+    prev();
+  }
+}
+
+const slider = $(".list"),
+      swipe = new Hammer($(".swipe"));
+
+slider.onclick = event => {
+  slide(event.target);
+}
+
+swipe.on("swipeleft", (ev) => {
+  next();
 });
-slider.addEventListener('mouseleave', _ => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mouseup', _ => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mousemove', e => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const SCROLL_SPEED = 3;
-  const walk = (x - startX) * SCROLL_SPEED;
-  slider.scrollLeft = scrollLeft - walk;
+
+swipe.on("swiperight", (ev) => {
+  prev();
 });
