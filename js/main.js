818120026ca4ecf242fdad76b770e0e5a488810d
Moonstress <lil.moonstress@gmail.com>
2023-04-25 14:09:02 -0400

    Inserted JS Quotation

--- js/main.js
+++ js/main.js
@@ -1,104 +1,37 @@
-document.getElementById("outer3").addEventListener("click", toggleState3);
+/*-----------QUOTATION--------*/
+
+function calculatePrice() {
+
+   while (!document.getElementById('addons0').checked && !document.getElementById('addons1').checked && !document.getElementById('addons2').checked && !document.getElementById('addons3').checked && !document.getElementById('addons4').checked) {
+      window.alert("You have not selected any Add ons number");
+      window.setTimeout(hideFieldMessages, 5000);
+
+   }
+
+   let paciColor = document.getElementById("paciColor");
+   let paciColorPrice = paciColor.options[paciColor.selectedIndex].value;
+
     
-function toggleState3() {
-  let galleryView = document.getElementById("galleryView")
-  let tilesView = document.getElementById("tilesView")
-  let outer = document.getElementById("outer3");
-  let slider = document.getElementById("slider3");
-  let tilesContainer = document.getElementById("tilesContainer");
-  if (slider.classList.contains("active")) {
-    slider.classList.remove("active");
-    outer.classList.remove("outerActive");
-    galleryView.style.display = "flex";
-    tilesView.style.display = "none";
-    
-    while (tilesContainer.hasChildNodes()) {
-      tilesContainer.removeChild(tilesContainer.firstChild)
-      }  
-  } else {
-    slider.classList.add("active");
-    outer.classList.add("outerActive");
-    galleryView.style.display = "none";
-    tilesView.style.display = "flex";
-     
-    for (let i = 0; i < imgObject.length - 1; i++) {
-      let tileItem = document.createElement("div");
-      tileItem.classList.add("tileItem");
-      tileItem.style.background =  "url(" + imgObject[i] + ")";
-      tileItem.style.backgroundSize = "contain";  
-      tilesContainer.appendChild(tileItem);      
-    }
-  };
-}
+   let decoLevel = document.getElementById("decoLevel");
+   let decoLevelPrice = decoLevel.options[decoLevel.selectedIndex].value;
 
-let imgObject = [
-  "https://placeimg.com/450/450/any",
-  "https://placeimg.com/450/450/animals",
-  "https://placeimg.com/450/450/architecture",
-  "https://placeimg.com/450/450/nature",
-  "https://placeimg.com/450/450/people",
-  "https://placeimg.com/450/450/tech",
-  "https://picsum.photos/id/1/450/450",
-  "https://picsum.photos/id/8/450/450",
-  "https://picsum.photos/id/12/450/450",
-  "https://picsum.photos/id/15/450/450",
-  "https://picsum.photos/id/5/450/450",
-];
-
-let mainImg = 0;
-let prevImg = imgObject.length - 1;
-let nextImg = 1;
-
-function loadGallery() {
-
-  let mainView = document.getElementById("mainView");
-  mainView.style.background = "url(" + imgObject[mainImg] + ")";
-
-  let leftView = document.getElementById("leftView");
-  leftView.style.background = "url(" + imgObject[prevImg] + ")";
-  
-  let rightView = document.getElementById("rightView");
-  rightView.style.background = "url(" + imgObject[nextImg] + ")";
-  
-  let linkTag = document.getElementById("linkTag")
-  linkTag.href = imgObject[mainImg];
-
-};
-
-function scrollRight() {
-  
-  prevImg = mainImg;
-  mainImg = nextImg;
-  if (nextImg >= (imgObject.length -1)) {
-    nextImg = 0;
-  } else {
-    nextImg++;
-  }; 
-  loadGallery();
-};
-
-function scrollLeft() {
-  nextImg = mainImg
-  mainImg = prevImg;
    
-  if (prevImg === 0) {
-    prevImg = imgObject.length - 1;
-  } else {
-    prevImg--;
-  };
-  loadGallery();
-};
+   /*if (decoLevelValue == "simple" ) {
+       decoLevelPrice = 25;    
 
-document.getElementById("navRight").addEventListener("click", scrollRight);
-document.getElementById("navLeft").addEventListener("click", scrollLeft);
-document.getElementById("rightView").addEventListener("click", scrollRight);
-document.getElementById("leftView").addEventListener("click", scrollLeft);
-document.addEventListener('keyup',function(e){
-    if (e.keyCode === 37) {
-    scrollLeft();
-  } else if(e.keyCode === 39) {
-    scrollRight();
-  }
-});
+   } else if (decoLevelValue == "normal") {
+      decoLevelPrice = 30;
+   }
+   else if (decoLevelValue == "premium") {
+      decoLevelPrice = 35;
+   }
 
-loadGallery();
+   else if (decoLevelValue == "deluxe") {
+      decoLevelPrice = 40;
+   }*/
+
+   let finalPrice = parseFloat(decoLevelPrice) + parseFloat(paciColorPrice);
+   alert (finalPrice);
+   /*document.getElementById("result").innerHTML == finalPrice;*/
+
+}
