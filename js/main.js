398c6e9b3bbabf50e4476c459a9c98ed8a3674d7
Moonstress <lil.moonstress@gmail.com>
2023-05-02 14:34:44 -0400

    Updated Javascript Quoting

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
 /*-----------QUOTATION--------*/
 
+//CLASES
 class Pacifier {
-   constructor(level, tier, addons,) {
+   constructor(level, tier, addons, addonsPrice, centerDif) {
       this.level = level;
       this.tier = tier;
       this.addons = addons;
+      this.addonsPrice = addonsPrice;
+      this.centerDif = centerDif;
    }
 }
 
+class Addons {
+   constructor(difficulty) {
+      this.difficulty = difficulty;      
+   }
+}
+
+const paci = new Pacifier("0");
+
+const addon1 = new Addons("0");
+const addon2 = new Addons("0");
+const addon3 = new Addons("0");
+const addon4 = new Addons("0");
+
+//FUNCIONES AUXILIARES
 //ALERT
 function alertUnselected() {
    while (!document.getElementById('addons0').checked && !document.getElementById('addons1').checked && !document.getElementById('addons2').checked && !document.getElementById('addons3').checked && !document.getElementById('addons4').checked) {
@@ -16,72 +33,112 @@ function alertUnselected() {
    }
 }
 
-//DECO LEVEL 
-function  decoLevelPrice ()  {
+//PACI LEVEL PRICE
+function calculatePaciLevel() {
    let decoLevel = document.getElementById("decoLevel");
    let decoLevelPrice = decoLevel.options[decoLevel.selectedIndex].value;
-   return decoLevelPrice;
-}
+   paci.level = decoLevelPrice;
+   return paci.level;
 
-decoLevelPrice = decoLevelPrice();
+}
 
 //TIER PRICE
 function calculateTierPrice() {
    let paciColor = document.getElementById("paciColor");
    let paciColorPrice = paciColor.options[paciColor.selectedIndex].value;
-   return paciColorPrice;
+   paci.tier = paciColorPrice;
+   return paci.tier;
 }
 
-let TierPrice = calculateTierPrice();
+//TIER PRICE
+function calculateCenterDif() {
+   let centerDif = document.getElementById("centerDif");
+   let centerDifPrice = centerDif.options[centerDif.selectedIndex].value;
+   paci.centerDif = centerDifPrice;
+   return paci.centerDif;
+}
 
-//ADD ONS DIFFICULTY
-let addonsDif = document.getElementsByClassName("addons");
-console.log(addonsDif.length);
+//CALCULATE ADD ONS
+
+function calculateAddons() {
+   if (document.getElementById('addons1').checked === true) {
+      paci.addons = 1;
+   } else if (document.getElementById('addons2').checked === true) {
+      paci.addons = 2;
+   } else if (document.getElementById('addons3').checked === true) {
+      paci.addons = 3;
+   } else if (document.getElementById('addons4').checked === true) {
+      paci.addons = 4;
+   }
+   return paci.addons;
+}
+
+//CALCULATE ADDONS DISCOUNT
+
+function calculateaddonsPrice() {
+   if ((paci.level == 25 || paci.level == 30) && paci.addons == 1) {
+      paci.addonsPrice = 2;
+   } else if ((paci.level == 25 || paci.level == 30) && paci.addons == 2) {
+      paci.addonsPrice = 4;
+   } else if ((paci.level == 25 || paci.level == 30) && paci.addons == 3) {
+      paci.addonsPrice = 6;
+   } else if ((paci.level == 25 || paci.level == 30) && paci.addons == 4) {
+      paci.addonsPrice = 8;
+   } else if (paci.level == 37 && paci.addons == 2) {
+      paci.addonsPrice = 2;
+   } else if (paci.level == 37 && paci.addons == 3) {
+      paci.addonsPrice = 4;
+   } else if (paci.level == 37 && paci.addons == 4) {
+      paci.addonsPrice = 6;
+   } else if (paci.level == 45 && paci.addons == 3) {
+      paci.addonsPrice = 2;
+   } else if (paci.level == 45 && paci.addons == 4) {
+      paci.addonsPrice = 4;
+   }
+   return paci.calculateaddonsPrice;
+}
+
+//Calculate First Addon Difficulty
+let addonsDif1 = document.getElementsByClassName("addons1");
+let addonsDif2 = document.getElementsByClassName("addons2");
+let addonsDif3 = document.getElementsByClassName("addons3");
+let addonsDif4 = document.getElementsByClassName("addons4");
+
+
+function calculateAddonsDifficulty(addon) {
+   for (let i = 0; i < addon.length; i++) {
+      if (addon[i].selected == true) {
+         let addonPrice = addon[i].value
+         return addonPrice;
+      }
+   }
+}
+
+// Set Addons Prices
+function setAddonsDif(){
+   addon1.difficulty = calculateAddonsDifficulty(addonsDif1);
+   addon2.difficulty = calculateAddonsDifficulty(addonsDif2);
+   addon3.difficulty = calculateAddonsDifficulty(addonsDif3);
+   addon4.difficulty = calculateAddonsDifficulty(addonsDif4);
+}
+
+function printPaci() {
+   calculatePaciLevel();
+   calculateTierPrice();
+   calculateCenterDif();
+   calculateAddons();
+   setAddonsDif();
+   calculateaddonsPrice(paci.addons);
+   console.log(paci);
+   console.log(addon1);
+   console.log(addon2);
+   console.log(addon3);
+   console.log(addon4);
+}
 
 function calculatePrice() {
-   
-   let decoAddonValue = 0;
-   let addon1True = document.getElementById('addons1').checked;
-   let addon2True = document.getElementById('addons2').checked;
-   let addon3True = document.getElementById('addons3').checked;
-   let addon4True = document.getElementById('addons4').checked;
-
-   if ((decoLevelPrice == 25 || decoLevelPrice == 30) && addon1True == true) {
-      decoAddonValue = 2;      
-   }
-   else if ((decoLevelPrice == 25 || decoLevelPrice == 30) && addon2True == true) {
-      decoAddonValue = 4;      
-
-   } else if ((decoLevelPrice == 25 || decoLevelPrice == 30) && addon3True == true) {
-      decoAddonValue = 6;   
-
-   }else if ((decoLevelPrice == 25 || decoLevelPrice == 30) && addon4True == true) {
-      decoAddonValue = 8;      
-
-   } else if (decoLevelPrice == 37 && addon2True == true) {
-      decoAddonValue = 2;      
-
-   } else if (decoLevelPrice == 37 && addon3True == true) {
-      decoAddonValue = 4;  
-
-   } else if (decoLevelPrice == 37 && addon5True == true) {
-      decoAddonValue = 4;   
-
-   } else if (decoLevelPrice == 45 && addon3True == true) {
-      decoAddonValue = 2;   
-
-   } else if (decoLevelPrice == 45 && addon4True == true) {
-      decoAddonValue = 4;    
-
-   } 
-
-   let decoValue = decoAddonValue;
-   let paci = new Pacifier(decoLevelPrice, TierPrice, decoValue);
-
-   let finalPrice = parseFloat(decoLevelPrice) + parseFloat(TierPrice)
-      + parseFloat(decoValue);
+   let finalPrice = parseInt(paci.level) + parseFloat(paci.tier) + parseFloat(paci.centerDif) + parseInt(paci.addonsPrice) + parseFloat(addon1.difficulty) + parseFloat(addon2.difficulty)  + parseFloat(addon3.difficulty) + parseFloat(addon4.difficulty);
    alert(finalPrice);
-   console.log(paci);
 }
 
 /*document.getElementById("result").innerHTML == finalPrice;*/
